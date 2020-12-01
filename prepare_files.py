c623ad191bb1900c57046881292f2cce2608ba97
yingyangle <cyang1999@gmail.com>
2020-12-01 15:27:05 -0500

    new files for bigger data

--- prepare_files.py
+++ prepare_files.py
@@ -9,10 +9,14 @@ wavscp = open('wav.scp', 'w')
 utt2spk = open('utt2spk', 'w')
 text = open('text', 'w')
 
+FOLDER = 'augmented-data-big'
+
+i = 0
+
 # for each speaker
-for speaker in ['19', '26', '27']:
+for speaker in [x for x in os.listdir(FOLDER) if not x.startswith('.')]:
 	
-	wavfiles = [x for x in os.listdir(join('augmented-data', speaker)) if x.endswith('.wav')]
+	wavfiles = [x for x in os.listdir(join(FOLDER, speaker)) if x.endswith('.wav')]
 	
 	# for each .wav file
 	for wavfile in wavfiles:
@@ -20,15 +24,19 @@ for speaker in ['19', '26', '27']:
 		utt = wavfile[:-4] # utterance id
 	
 		# write file info to wav.scp
-		wavscp.write(utt + ' ' + join('augmented-data', speaker, wavfile) + '\n')
+		wavscp.write(utt + ' ' + join(FOLDER, speaker, wavfile) + '\n')
 		# write speaker info to utt2spk
 		utt2spk.write(utt + ' ' + speaker + '\n')
 		# write transcription to text
-		with open(join('augmented-data', speaker, utt+'.lab')) as ein:
+		with open(join(FOLDER, speaker, utt+'.lab')) as ein:
 			text.write(utt + ' ' + ein.read() + '\n')
 			
-		print(wavfile)
+		# print(wavfile)
+		i += 1
+	
+	print(speaker)
 
+print('total', i)
 
 wavscp.close()
 utt2spk.close()
